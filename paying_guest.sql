f4181a6929614a32f5377083e952e6e8f8d1d574
Navaneetha007 <115872551+Navaneetha007@users.noreply.github.com>
2023-11-27 17:31:34 +0530

    paying_guest.sql

--- paying_guest.sql
+++ paying_guest.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jun 10, 2019 at 10:31 AM
--- Server version: 10.1.29-MariaDB
--- PHP Version: 7.1.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -13,55 +6,28 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `hostel`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
---
 
 CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(300) NOT NULL,
-  `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, 
   `updation_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
---
--- Dumping data for table `admin`
---
 
 INSERT INTO `admin` (`id`, `username`, `email`, `password`, `reg_date`, `updation_date`) VALUES
 (1, 'admin', 'anuj.lpu1@gmail.com', 'Test@1234', '2023-04-04 20:31:45', '2023-04-17');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `adminlog`
---
 
 CREATE TABLE `adminlog` (
   `id` int(11) NOT NULL,
   `adminid` int(11) NOT NULL,
   `ip` varbinary(16) NOT NULL,
   `logintime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `courses`
---
 
 CREATE TABLE `courses` (
   `id` int(11) NOT NULL,
@@ -69,11 +35,9 @@ CREATE TABLE `courses` (
   `course_sn` varchar(255) DEFAULT NULL,
   `course_fn` varchar(255) DEFAULT NULL,
   `posting_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ;
 
---
--- Dumping data for table `courses`
---
+
 
 INSERT INTO `courses` (`id`, `course_code`, `course_sn`, `course_fn`, `posting_date`) VALUES
 (1, 'B10992', 'B.Tech', 'Bachelor  of Technology', '2023-04-11 19:31:42'),
@@ -84,11 +48,7 @@ INSERT INTO `courses` (`id`, `course_code`, `course_sn`, `course_fn`, `posting_d
 (6, 'MBA75', 'MBA', 'Master of Business Administration', '2023-04-11 19:34:59'),
 (7, 'BE765', 'BE', 'Bachelor of Engineering', '2023-04-11 19:35:19');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `registration`
---
 
 CREATE TABLE `registration` (
   `id` int(11) NOT NULL,
@@ -120,23 +80,16 @@ CREATE TABLE `registration` (
   `pmntPincode` int(11) DEFAULT NULL,
   `postingDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updationDate` varchar(500) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ;
 
---
--- Dumping data for table `registration`
---
+
 
 INSERT INTO `registration` (`id`, `roomno`, `seater`, `feespm`, `foodstatus`, `stayfrom`, `duration`, `course`, `regno`, `firstName`, `middleName`, `lastName`, `gender`, `contactno`, `emailid`, `egycontactno`, `guardianName`, `guardianRelation`, `guardianContactno`, `corresAddress`, `corresCIty`, `corresState`, `corresPincode`, `pmntAddress`, `pmntCity`, `pmnatetState`, `pmntPincode`, `postingDate`, `updationDate`) VALUES
 (6, 100, 5, 8000, 0, '2023-04-22', 5, 'Bachelor  of Technology', 10806121, 'Anuj', '', 'kumar', 'male', 8285703354, 'anuj.lpu1@gmail.com', 0, 'XYZ', 'Mother', 8285703354, 'New Delhi India', 'Aligarh', 'Uttar Pradesh', 202001, 'New Delhi India', 'Delhi', 'Delhi (NCT)', 202001, '2023-04-16 08:24:09', ''),
 (7, 100, 5, 8000, 1, '2023-06-17', 4, 'Bachelor of Engineering', 108061211, 'Test', 'test', 'kumar', 'male', 8467067344, 'test@gmail.com', 123456789, 'test', 'test', 1236547890, 'New Delhi India', 'Aligarh', 'Uttar Pradesh', 202001, 'New Delhi India', 'Delhi', 'Delhi (NCT)', 202001, '2023-06-23 11:54:35', ''),
 (8, 112, 3, 4000, 0, '2023-06-27', 5, 'Bachelor  of Science', 102355, 'rahul', 'kumar', 'Singh', 'male', 6786786786, 'rahul@gmail.com', 789632587, 'demo', 'demo', 1234567890, 'New Delhi', 'Delhi', 'Delhi (NCT)', 110001, 'New Delhi', 'Delhi', 'Delhi (NCT)', 110001, '2023-06-26 16:31:08', ''),
-(9, 132, 5, 2000, 1, '2023-06-28', 6, 'Bachelor of Engineering', 586952, 'Ajay', '', 'kumar', 'male', 8596185625, 'ajay@gmail.com', 8285703354, 'demo', 'demo', 8285703354, 'New Delhi India', 'Aligarh', 'Uttar Pradesh', 202001, 'New Delhi India', 'Delhi', 'Delhi (NCT)', 202001, '2023-06-26 16:40:07', '');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `rooms`
---
 
 CREATE TABLE `rooms` (
   `id` int(11) NOT NULL,
@@ -144,11 +97,8 @@ CREATE TABLE `rooms` (
   `room_no` int(11) DEFAULT NULL,
   `fees` int(11) DEFAULT NULL,
   `posting_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
---
--- Dumping data for table `rooms`
---
 
 INSERT INTO `rooms` (`id`, `seater`, `room_no`, `fees`, `posting_date`) VALUES
 (1, 5, 100, 8000, '2023-04-11 22:45:43'),
@@ -157,20 +107,13 @@ INSERT INTO `rooms` (`id`, `seater`, `room_no`, `fees`, `posting_date`) VALUES
 (4, 3, 112, 4000, '2023-04-12 01:31:07'),
 (5, 5, 132, 2000, '2023-04-12 01:31:15');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `states`
---
 
 CREATE TABLE `states` (
   `id` int(11) NOT NULL,
   `State` varchar(150) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+);
 
---
--- Dumping data for table `states`
---
 
 INSERT INTO `states` (`id`, `State`) VALUES
 (1, 'Andaman and Nicobar Island (UT)'),
@@ -210,11 +153,7 @@ INSERT INTO `states` (`id`, `State`) VALUES
 (35, 'Uttar Pradesh'),
 (36, 'West Bengal');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `userlog`
---
 
 CREATE TABLE `userlog` (
   `id` int(11) NOT NULL,
@@ -224,11 +163,8 @@ CREATE TABLE `userlog` (
   `city` varchar(255) NOT NULL,
   `country` varchar(255) NOT NULL,
   `loginTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
---
--- Dumping data for table `userlog`
---
 
 INSERT INTO `userlog` (`id`, `userId`, `userEmail`, `userIp`, `city`, `country`, `loginTime`) VALUES
 (1, 10, 'test@gmail.com', '', '', '', '2023-06-22 06:16:42'),
@@ -241,11 +177,7 @@ INSERT INTO `userlog` (`id`, `userId`, `userEmail`, `userIp`, `city`, `country`,
 (9, 10, 'test@gmail.com', 0x3a3a31, '', '', '2019-06-10 07:17:32'),
 (10, 10, 'test@gmail.com', 0x3a3a31, '', '', '2019-06-10 08:08:59');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `userregistration`
---
 
 CREATE TABLE `userregistration` (
   `id` int(11) NOT NULL,
@@ -260,112 +192,126 @@ CREATE TABLE `userregistration` (
   `regDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updationDate` varchar(45) DEFAULT NULL,
   `passUdateDate` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
---
--- Dumping data for table `userregistration`
---
+
 
 INSERT INTO `userregistration` (`id`, `regNo`, `firstName`, `middleName`, `lastName`, `gender`, `contactNo`, `email`, `password`, `regDate`, `updationDate`, `passUdateDate`) VALUES
 (10, '108061211', 'Test', 'test', 'kumar', 'male', 1234567890, 'test@gmail.com', 'Test@123', '2023-06-22 04:21:33', '10-06-2019 12:48:13', NULL),
 (19, '102355', 'rahul', 'kumar', 'Singh', 'male', 6786786786, 'rahul@gmail.com', '6786786786', '2023-06-26 16:33:36', '', ''),
 (20, '586952', 'Ajay', '', 'kumar', 'male', 8596185625, 'ajay@gmail.com', '8596185625', '2023-06-26 16:40:07', '', '');
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `admin`
---
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `courses`
---
+
 ALTER TABLE `courses`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `registration`
---
+
 ALTER TABLE `registration`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `rooms`
---
+
 ALTER TABLE `rooms`
   ADD PRIMARY KEY (`id`),
   ADD KEY `room_no` (`room_no`);
 
---
--- Indexes for table `states`
---
+
 ALTER TABLE `states`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `userlog`
---
+
 ALTER TABLE `userlog`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `userregistration`
---
+
 ALTER TABLE `userregistration`
   ADD PRIMARY KEY (`id`),
   ADD KEY `email` (`email`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `admin`
---
 ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT for table `courses`
---
+
 ALTER TABLE `courses`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
---
--- AUTO_INCREMENT for table `registration`
---
+
 ALTER TABLE `registration`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
---
--- AUTO_INCREMENT for table `rooms`
---
+
 ALTER TABLE `rooms`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
---
--- AUTO_INCREMENT for table `states`
---
 ALTER TABLE `states`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
---
--- AUTO_INCREMENT for table `userlog`
---
+
 ALTER TABLE `userlog`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
---
--- AUTO_INCREMENT for table `userregistration`
---
+
 ALTER TABLE `userregistration`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SELECT course COUNT(*) AS student_count FROM registration GROUP BY course;
+
+
+SELECT seater, COUNT(*) AS room_count, AVG(feespm) AS avg_fees FROM registration GROUP BY seater;
+
+SELECT * FROM userlog WHERE loginTime > (SELECT loginTime FROM userlog WHERE useremail = 'liya@gmail.com');
+
+DELIMITER //
+
+CREATE TRIGGER after_reg_insert AFTER INSERT ON registration
+FOR EACH ROW 
+BEGIN 
+	INSERT INTO userinfo (ussername, logged_in)
+    VALUES (NEW.id, NOW());
+END;
+
+//
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE FUNCTION CalculateTotalFees(IN student_id INT)
+RETURNS INT
+BEGIN
+    DECLARE total_fees INT;
+
+    SELECT SUM(feespm) INTO total_fees FROM registration WHERE id = student_id;
+
+    RETURN total_fees;
+END //
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE GetStudentInfo(IN student_id INT)
+BEGIN
+    SELECT * FROM registration WHERE id = student_id;
+END //
+
+DELIMITER ;
+
+
+SELECT
+    r.firstName,
+    r.regno,
+    r.contactno,
+    reg.roomno,
+    reg.seater,
+    reg.stayfrom
+FROM
+    registration reg
+JOIN
+    userregistration r ON reg.regno = r.regNo;
+
